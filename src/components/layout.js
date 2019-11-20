2501a24c5bdbad7aef5c39c694d42d5c0906b3f5
Daniel Dawidowski <dvds1987@gmail.com>
2019-11-20 15:32:37 +0100

    Querying Data with GraphQL

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,9 +1,4 @@
-/**
- * Layout component that queries for data
- * with Gatsby's useStaticQuery component
- *
- * See: https://www.gatsbyjs.org/docs/use-static-query/
- */
+
 
 import React from "react"
 import PropTypes from "prop-types"
@@ -12,16 +7,19 @@ import { useStaticQuery, graphql } from "gatsby"
 import Header from "./header"
 import "./layout.css"
 
-const Layout = ({ children }) => {
-  const data = useStaticQuery(graphql`
-    query SiteTitleQuery {
-      site {
-        siteMetadata {
-          title
-        }
-      }
+const getSiteMetadata = graphql`
+{
+  site {
+    siteMetadata {
+      title
+      author
+      createdAt
     }
-  `)
+  }
+}`
+
+const Layout = ({ children }) => {
+  const data = useStaticQuery(getSiteMetadata)
 
   return (
     <>
@@ -36,9 +34,9 @@ const Layout = ({ children }) => {
       >
         <main>{children}</main>
         <footer>
-          © {new Date().getFullYear()}, Built with
-          {` `}
-          <a href="https://www.gatsbyjs.org">Gatsby</a>
+           Built by {' '}
+           {data.site.siteMetadata.author}, Created {' '}
+           {data.site.siteMetadata.createdAt}
         </footer>
       </div>
     </>
